6fbdd0cc07a5e7061d7946af344f11ac0bd12381
streetstreet0 <streetstreet.kjewiss@gmail.com>
2024-03-07 17:14:50 +1300

    now working on the response. For the time being, it will be a html resposne

--- Request.cs
+++ Request.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,38 @@ namespace CSharpServer
         private String Url { get; set; }
         private String Host { get; set; }
 
-
+        /**
+         * Private constructor to create a request object.
+         * 
+         * @param type The type of request.
+         * @param url The url of the server.
+         * @param host The host of the server.
+         */
         private Request(String type, String url, String host)
         {
-            Type = type;
-            Url = url;
-            Host = host;
+            this.Type = type;
+            this.Url = url;
+            this.Host = host;
+            TestRequest();
         }
 
+        /**
+         * Display the variables of the request to the debug console.
+         * 
+         */
+        private void TestRequest()
+        {
+            Debug.WriteLine("Type = " + Type);
+            Debug.WriteLine("url = " + Url);
+            Debug.WriteLine("host = " + Host);
+        }
+
+        /**
+         * Generate a request object from a string request.
+         * 
+         * @param request The String request.
+         * @return The generated request object.
+         */
         public static Request GetRequest(String request)
         {
             if (String.IsNullOrEmpty(request))
@@ -28,7 +53,10 @@ namespace CSharpServer
             }
 
             String[] tokens = request.Split(' ');
-            return new Request("", "", "");
+            String type = tokens[0];
+            String url = tokens[1];
+            String host = tokens[4];
+            return new Request(type, url, host);
         }
     }
 }
--- Response.cs
+++ Response.cs
@@ -3,11 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
 
 namespace CSharpServer
 {
     public class Response
     {
+        private Byte[] ByteData;
+        private String Status;
+        private String InputDataType;
 
+        /**
+         * Private constructor to create a Response object
+         * 
+         * @param byteData The data to be sent in the response.
+         * @param status The current status of the server.
+         * @param inputDataType The type of data send in the request.
+          */
+        private Response(String status, String inputDataType, Byte[] byteData)
+        {
+            this.ByteData = byteData;
+            this.Status = status;
+            this.InputDataType = inputDataType;
+        }
+
+        /**
+         * Create a new Response for a given request
+         * 
+         * @param request The request to respond to
+         */
+        public static Response RespondTo(Request request)
+        {
+            if (request == null)
+            {
+                return CreateNullRequest();
+            }
+
+            return null;
+        }
+
+        /**
+         * Create a Response for a null request
+         * 
+         */
+        public static Response CreateNullRequest()
+        {
+            return new Response("400 Bad Request", "text/html", new byte[0]);
+        }
+
+        /**
+         * Post data back to the client.
+         * 
+         */
+        public void Post(NetworkStream outputStream)
+        {
+            StreamWriter writer = new StreamWriter(outputStream);
+
+            writer.WriteLine(HTTPSserver.VERSION + " " + Status);
+            writer.WriteLine("Server: " + HTTPSserver.NAME);
+            writer.WriteLine("Content-Type: " + InputDataType);
+            writer.WriteLine("Accept-Ranges: bytes");
+            writer.WriteLine("Content-Length: " + ByteData.Length);
+
+            outputStream.Write(ByteData, 0, ByteData.Length);
+        }
     }
 }
